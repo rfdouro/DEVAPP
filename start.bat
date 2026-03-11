0b4c1c56505ba96954a3f40bd9e46d2a50ddcd5c
Rômulo F. Douro <rfdouro@gmail.com>
2026-03-11 08:33:06 -0300

    Update lastcommit format in start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 chcp 65001
 
-set lastcommit=$Format:%ad --date=format:%d/%m/%Y %H:%M$
+set lastcommit=$Format:%ai$
 set author=$Format:%an$
 
 set now=%date:~6,4%-%date:~3,2%-%date:~0,2% %time:~0,2%:%time:~3,2%
